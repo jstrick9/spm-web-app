d48120ddcadc8e17cb73751b425d8d3af531f32c
jstrick9 <95830053+jstrick9@users.noreply.github.com>
2026-06-02 17:29:39 -0400

    Delete fix_stage_bug.js

--- fix_stage_bug.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const path = 'spm-web-app/wedding-app/client/src/screens/events/layouts/CanvasPage.tsx';
-let code = fs.readFileSync(path, 'utf8');
-
-// I can see the issue: There are two <Stage elements maybe, or two onDragMoves.
-// Let's inspect line 220-235
