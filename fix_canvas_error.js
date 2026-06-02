dd9fc874ec25534e1ca021dfecf43d586250337b
jstrick9 <95830053+jstrick9@users.noreply.github.com>
2026-06-02 16:57:03 -0400

    Delete fix_canvas_error.js

--- fix_canvas_error.js
+++
@@ -1,6 +0,0 @@
-const fs = require('fs');
-
-const path = 'spm-web-app/wedding-app/client/src/screens/events/layouts/CanvasPage.tsx';
-let code = fs.readFileSync(path, 'utf8');
-
-// I might have replaced incorrectly. Let me just find out what line 240 is.
