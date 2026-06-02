fa8bd01ab7b3cb69b13402359658dccc152de7d1
jstrick9 <95830053+jstrick9@users.noreply.github.com>
2026-06-02 17:25:55 -0400

    Delete fix_quotes5.js

--- fix_quotes5.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const path2 = 'spm-web-app/wedding-app/client/src/screens/events/vendors/VendorTimelineChart.tsx';
-let code2 = fs.readFileSync(path2, 'utf8');
-
-code2 = code2.replace(/title=\{\`\\\$\{span\.title\} \(\\\$\{format\(parseISO\(span\.starts_at\), 'h:mm a'\)\}\)\`\}/g, "title={`${span.title} (${format(parseISO(span.starts_at), 'h:mm a')})`}");
-
-fs.writeFileSync(path2, code2);
