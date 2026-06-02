1ce96a7c4b84e5eebe52af06043c1d1f3a336ec8
jstrick9 <95830053+jstrick9@users.noreply.github.com>
2026-06-02 17:36:09 -0400

    Delete fix_vite_syntax.js

--- fix_vite_syntax.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-
-const path = 'spm-web-app/wedding-app/client/vite.config.ts';
-let code = fs.readFileSync(path, 'utf8');
-
-code = code.replace("    })],,", "    })],");
-
-fs.writeFileSync(path, code);
